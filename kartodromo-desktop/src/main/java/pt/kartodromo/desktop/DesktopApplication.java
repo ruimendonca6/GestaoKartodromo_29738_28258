8aede28a868a72948ba75d967a41827bf3e0101f
Rui Mendonça <ruiagmendonca@gmail.com>
2026-04-26 00:43:53 +0100

    style(desktop): apply FlatLaf theme

--- kartodromo-desktop/src/main/java/pt/kartodromo/desktop/DesktopApplication.java
+++ kartodromo-desktop/src/main/java/pt/kartodromo/desktop/DesktopApplication.java
@@ -1,6 +1,9 @@
 package pt.kartodromo.desktop;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
 
 import pt.kartodromo.core.config.HibernateUtil;
 import pt.kartodromo.desktop.ui.KartodromoDesktopFrame;
@@ -9,6 +12,15 @@ public class DesktopApplication {
 
     public static void main(String[] args) {
         Runtime.getRuntime().addShutdownHook(new Thread(HibernateUtil::shutdown));
-        SwingUtilities.invokeLater(() -> new KartodromoDesktopFrame().setVisible(true));
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            } catch (Exception e) {
+                System.err.println("Erro ao aplicar tema FlatLaf.");
+            }
+
+            new KartodromoDesktopFrame().setVisible(true);
+        });
     }
 }
